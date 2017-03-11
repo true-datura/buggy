928f025c73c23875fff2d4d5cb2f257976948ea2
true-datura <grrrr@protonmail.com>
2017-03-11 22:42:09 +0200

    Joined load of related posts in tags processor

--- buggy/context_processors.py
+++ buggy/context_processors.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 """Context processors."""
+from sqlalchemy.orm import joinedload
+
 from buggy.extensions import cache
 from buggy.post.models import Tag
 
@@ -7,6 +9,7 @@ from buggy.post.models import Tag
 @cache.cached(timeout=60 * 30, key_prefix='all_tags')
 def tags_processor():
     """15 most popular tags with posts reference counters."""
-    tags = [(tag.name, len(tag.related_posts)) for tag in Tag.query.all()]
+    tags = [(tag.name, len(tag.related_posts))
+            for tag in Tag.query.options(joinedload('related_posts')).all()]
     tags.sort(key=lambda x: x[1], reverse=True)
     return dict(tags=tags[:15])
